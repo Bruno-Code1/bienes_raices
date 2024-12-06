49b03f6d5a7ac639331dd4361ad70d9f009e989e
Bruno Bettio <bruno.web.bpage@gmail.com>
2024-12-06 14:17:18 -0300

    agrego codigo para actualizar  propiedades

--- admin/index.php
+++ admin/index.php
@@ -21,6 +21,8 @@
     <h1>Administrador de Bienes Raíces</h1>
     <?php if(intval($resultado) === 1) :?>
       <p class="alerta exito">Anuncio creado correctamente</p>
+    <?php elseif(intval($resultado) === 2): ?>
+      <p class="alerta exito">Anuncio actualizado correctamente</p>
     <?php endif; ?>
     <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
   
@@ -43,7 +45,7 @@
           <td>$<?php echo $propiedad['precio']; ?></td>
           <td>
             <a href="#" class="boton-rojo-block">Eliminar</a>
-            <a href="#" class="boton-amarillo-block">Actualizar</a>
+            <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
           </td>
         </tr>
         <?php endwhile; ?>
